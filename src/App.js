329650b62de6176d13652e8f48695694c496292e
Odysseus <drsprogramming2020@gmail.com>
2022-08-10 12:04:28 +0430

    Add ProductCard Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import NavBar from "./Components/navBar";
 
 function App() {
-    return (
+
+    return(
         <>
-            <NavBar/>
-            <div style={{width: "100%", height: 2000, marginTop: 64}}/>
         </>
     );
 }
---
+++ src/Components/productCard.jsx
@@ -0,0 +1,43 @@
+import '../Styles/Components/productCard.css';
+import React from 'react';
+import {Button, IconButton, useMediaQuery} from "@mui/material";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+
+function ProductCard({name, price, image}) {
+    const data = {
+        name: "Paper Mario the Origami King",
+        price: "1,300,000",
+        image: require('../Assets/product.png')
+    }
+
+    const matches = useMediaQuery('(min-width: 1024px)');
+
+    return (
+        <>
+            <div className="card-root card-shadow">
+                <div className="card-container">
+                    <img src={data.image} className="product-image" alt="product"/>
+                    <div className="product-info">
+                        <div className="product-name">{data.name}</div>
+                        <div className="product-price">{data.price} تومان</div>
+                        <div className="product-commands">
+                            <IconButton style={{color: "#FF5D5D"}}>
+                                <FavoriteBorderIcon htmlColor="#FF5D5D"/>
+                            </IconButton>
+                            {matches ?
+                                <Button style={{borderRadius: 10, fontFamily: '"B Yekan", sans-serif', color: '#0080FF'}}
+                                        variant="outlined"
+                                        endIcon={<ShoppingCartOutlinedIcon style={{minWidth: 30}}/>}>خرید</Button>
+                                : <IconButton style={{color: "#0080FF"}}>
+                                    <ShoppingCartOutlinedIcon htmlColor="#0080FF"/>
+                                </IconButton>}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default ProductCard;
